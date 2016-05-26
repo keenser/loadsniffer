eaf68f543991378537ffeb0229c7db277a5a2a7d
German Skalauhov <keenser.sg@gmail.com>
2016-05-26 18:51:00 +0300

    1

--- background.js
+++ background.js
@@ -1,93 +1,120 @@
-var verboseLevel = 2; // 0: nothing, 1: extension init and errors, 2: every request, nicely formatted, 3: lots of details
 var urllib = [];
 var tabid = null;
-//var onBeforeRequestListener = function(details) {
-//  console.log("    in onBeforeRequestListener listener: "+EXACT(details.method)+" "+EXACT(details.url)+" "+EXACT(details.type)+" -> "+details.statusCode);
-//  return null;
-//}
 
-chrome.extension.onMessage.addListener(function(request, sender, f_callback){
-  if (request.action == 'tabid') {
-	console.log('request tabid info for', request);
-	tabid = request.tabid;
-	f_callback(urllib[request.tabid]); //обратное сообщение
- }
- else if (request.action == 'url') {
-	console.log('request url', request.url);
- }
+chrome.extension.onMessage.addListener(function(request, sender, f_callback) {
+    if (request.action == 'tabid') {
+        console.log('request tabid info for', request);
+        tabid = request.tabid;
+        console.log('return tabId', urllib[request.tabid] || [])
+        f_callback(urllib[request.tabid] || []);
+    } else if (request.action == 'url') {
+        console.log('request url', request.url);
+    }
 });
 
-var get=function(url,callback){
-  var xmlRequest=new XMLHttpRequest();
-  xmlRequest.open('GET',url,true);
-  //xmlRequest.overrideMimeType('text/xml');
-  xmlRequest.send();
+var get = function(url, callback) {
+        var xmlRequest = new XMLHttpRequest();
+        xmlRequest.open('GET', url, true);
+        xmlRequest.send();
 
-  xmlRequest.onreadystatechange=function(){
-    console.log("get", xmlRequest);
-    if(xmlRequest.readyState==4){
-      callback(xmlRequest.responseXML);
-    }
-  };
-};
+        xmlRequest.onreadystatechange = function() {
+            if (xmlRequest.readyState == 4) {
+                callback(xmlRequest.responseXML);
+            }
+        };
+    };
 
 var onResponseStartedListener = function(details) {
-  if (details.tabId == -1) {
-    return null;
-  }
-  var type = details.type;
-//  if (type !== "other" && type !== "object") {
-//    return null;
-//  }
-  for (var i = 0; i < details.responseHeaders.length; ++i) {
-    if (details.responseHeaders[i].name.toLowerCase() === 'content-type') {
-      type = details.responseHeaders[i].value;
-      break;
-    }
-  }
-  console.log("onResponseStarted listener: "+details.tabId+" "+details.method+" "+details.url+" "+details.type+" "+type+" -> "+details.statusCode);
-  get(details.url,function(xml) {
-     console.log('xml', xml);
-  });
-  (urllib[details.tabId] = urllib[details.tabId] || []).push(details.url);
-  if ( details.tabId == tabid) {
-  chrome.extension.sendMessage(
-    { action: "urllib", urllib: urllib[details.tabId]}
- );
-  }
-  return null;
-};  // onResponseStartedListener
+        if (details.tabId == -1) {
+            return null;
+        }
+        urllib[details.tabId] = urllib[details.tabId] || []
+        var type;
+        for (var i = 0; i < details.responseHeaders.length; ++i) {
+            if (details.responseHeaders[i].name.toLowerCase() === 'content-type') {
+                type = details.responseHeaders[i].value;
+                break;
+            }
+        }
+        console.log("onResponseStarted listener: " + details.tabId + " " + details.method + " " + details.url + " " + details.type + " " + type + " -> " + details.statusCode);
+            urllib[details.tabId].push( {
+                url: details.url,
+                bitrate: []
+            });
+        if (details.tabId == tabid) {
+            chrome.extension.sendMessage({
+                action: "urllib",
+                urllib: urllib[details.tabId]
+            });
+        }
+        return null;
+    };
 
-chrome.webRequest.onResponseStarted.addListener(onResponseStartedListener,
-  {
-    urls:[
-//"*://*/*track*",
-"*://*/*.mp4*",
-//"*://*/*video*",
-"*://*/*.f4m*",
-"*://*.youtube.com/embed/*",
-"*://*.youtube.com/watch*",
-//   "<all_urls>"
+var f4mListener = function(details) {
+        if (details.tabId == -1) {
+            return null;
+        }
+        urllib[details.tabId] = urllib[details.tabId] || []
+        var type;
+        for (var i = 0; i < details.responseHeaders.length; ++i) {
+            if (details.responseHeaders[i].name.toLowerCase() === 'content-type') {
+                type = details.responseHeaders[i].value;
+                break;
+            }
+        }
+        console.log("f4mListener: " + details.tabId + " " + details.method + " " + details.url + " " + details.type + " " + type + " -> " + details.statusCode);
+        get(details.url, function(xml) {
+            console.log('xml', xml);
+            media = xml.getElementsByTagName('media');
+            bitrate = []
+            for (var i = 0; i < media.length; i++) {
+                console.log(details.tabId, media[i].getAttribute("url"), media[i].getAttribute("bitrate"));
+                bitrate.push({
+                    url: media[i].getAttribute("url"),
+                    bitrate: media[i].getAttribute("bitrate")
+                })
+            }
+            urllib[details.tabId].push( {
+                url: details.url,
+                bitrate: bitrate
+            });
+            if (details.tabId == tabid) {
+                chrome.extension.sendMessage({
+                    action: "urllib",
+                    urllib: urllib[details.tabId]
+                });
+            }
+
+        });
+        return null;
+    }; // onResponseStartedListener
+chrome.webRequest.onResponseStarted.addListener(onResponseStartedListener, {
+    urls: [
+    //"*://*/*track*",
+    "*://*/*.mp4*",
+    //"*://*/*video*",
+    "*://*.youtube.com/embed/*", "*://*.youtube.com/watch*",
+    //   "<all_urls>"
     ],
-  }
-, ["responseHeaders"]);  // options: responseHeaders
-
-chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
-   if (changeInfo.status == 'loading') {
-     console.log('reload tabid:', tabId, changeInfo);
-     urllib[tabId] = [];
-   }
+}, ["responseHeaders"]); // options: responseHeaders
+chrome.webRequest.onResponseStarted.addListener(f4mListener, {
+    urls: ["*://*/*.f4m*", ],
+}, ["responseHeaders"]); // options: responseHeaders
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status == 'loading' && typeof changeInfo.url !== 'undefined') {
+        console.log('reload tabid:', tabId, changeInfo);
+        urllib[tabId] = [];
+    }
 });
 
 var onStartupOrOnInstalledListener = function() {
-};  // onStartupOrOnInstalledListener
+        console.log("onStartupOrOnInstalledListener");
+    };
+
 chrome.runtime.onStartup.addListener(function() {
-  if (verboseLevel >= 1) console.log("        in onStartup listener");
-  onStartupOrOnInstalledListener();
-  if (verboseLevel >= 1) console.log("        out onStartup listener");
+    onStartupOrOnInstalledListener();
 });
+
 chrome.runtime.onInstalled.addListener(function() {
-  if (verboseLevel >= 1) console.log("        in onInstalled listener");
-  onStartupOrOnInstalledListener();
-  if (verboseLevel >= 1) console.log("        out onInstalled listener");
-});  // onInstalled listener
+    onStartupOrOnInstalledListener();
+});
--- popup.js
+++ popup.js
@@ -1,40 +1,64 @@
-chrome.tabs.getSelected(null,function(tab) {
-  console.log('tabs',tab);
-  chrome.extension.sendMessage(
-    { action: "tabid", tabid: tab.id},
-    function(urllib){
-        console.log('tabs.getSelected.backMessage:', urllib);
-        message.innerText = urllib;
-    }
- );
+	var addSingleLink = function(tr, index, linkSource) {
+			var td = document.createElement("td");
+			td.setAttribute('rule', 'url');
+			td.textContent = linkSource.url;
+			tr.appendChild(td)
+	        for (var i = 0; i < linkSource.bitrate.length; i++) {
+				var td = document.createElement("td");				
+				td.setAttribute('rule', 'bitrate');
+				var a = document.createElement("a");
+				a.textContent = linkSource.bitrate[i].bitrate;
+				a.setAttribute("href", linkSource.bitrate[i].url);
+				a.setAttribute('download', linkSource.bitrate[i].url);
+				td.appendChild(a)
+				tr.appendChild(td)
+			}
+	    };
 
-});
+	var addLinks = function(videoLinks) {
+	        var container = document.getElementById("content");
+	        var table = document.createElement("table");
+	        for (var i = 0; i < videoLinks.length; ++i) {
+	            var tr = document.createElement("tr");
+	            tr.setAttribute('index', i);
+	            addSingleLink(tr, i, videoLinks[i]);
+	            table.appendChild(tr);
+	        }
+            container.appendChild(table);
+	    };
 
-chrome.extension.onMessage.addListener(function(request, sender) {
-  if ( request.action == 'urllib') {
-  console.log('popup onMessage',request);
-  var message = document.querySelector('#message');
-  message.innerText = request.urllib;
- }
-});
 
-//function onWindowLoad() {
-//
-//chrome.tabs.getSelected(null,function(tab) {
-//  console.log('tabs',tab);
-//});
+	chrome.tabs.getSelected(null, function(tab) {
+	    chrome.extension.sendMessage({
+	        action: "tabid",
+	        tabid: tab.id
+	    }, function(urllib) {
+	        addLinks(urllib);
+	    });
+	});
 
-//  var message = document.querySelector('#message');
-//  message.innerText = '11';
-//  console.log('1:', _tabid);
-//  chrome.extension.sendMessage(
-//    { action: "getSource", tabid: _tabid},
-//    function(backMessage){
-//	console.log('2. popup onWindowLoad backMessage:', backMessage);
-//        message.innerText = backMessage +" "+ _tabid;
-//    }
-// );
-// console.log('3');
-//}
+	chrome.extension.onMessage.addListener(function(request, sender) {
+	    if (request.action == 'urllib') {
+	        addLinks(request.urllib);
+	    }
+	});
 
-//window.onload = onWindowLoad;
+	//function onWindowLoad() {
+	 //  var message = document.querySelector('#message');
+	 //
+	 //chrome.tabs.getSelected(null,function(tab) {
+	 //  console.log('tabs',tab);
+	 //});
+	 //  message.innerText = '11';
+	 //  console.log('1:', _tabid);
+	 //  chrome.extension.sendMessage(
+	 //    { action: "getSource", tabid: _tabid},
+	 //    function(backMessage){
+	 //	console.log('2. popup onWindowLoad backMessage:', backMessage);
+	 //        message.innerText = backMessage +" "+ _tabid;
+	 //    }
+	 // );
+	 // console.log('3');
+	 //}
+	 //window.onload = onWindowLoad;
+	
