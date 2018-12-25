4ef06d81da0506916ca13660af8e71d1999f18d5
keenser <keenser.sg@gmail.com>
2018-12-26 00:25:05 +0300

    mrc.py run youtube-dl in CancellablePool with timeout

--- mrc.py
+++ mrc.py
@@ -5,30 +5,32 @@
 # Media Renderer control server
 
 import asyncio
-import aioupnp
-import aiohttp
-import aiofiles
 import json
 import urllib.parse
 import os.path
-import torrentstream
 import logging
 import logging.handlers
 import mimetypes
+#import async_timeout
+import multiprocessing
 import youtube_dl
+#import aiofiles
+import aiohttp
+import aioupnp
+import torrentstream
 
 #delete generic extractor
 youtube_dl.extractor._ALL_CLASSES.pop()
 
-class MediaDevice(object):
+class MediaDevice:
     def __init__(self, device):
         self.media = device
         self.status = {'state': None, 'item': [], 'device': device.friendlyName}
 
     def __repr__(self):
-        return "{} {} {}".format(device, status)
+        return "{} {}".format(self.media, self.status)
 
-class UPnPctrl(object):
+class UPnPctrl:
     def __init__(self, loop=None, http=None, httpport=0):
         self.log = logging.getLogger(self.__class__.__name__)
         self.aioupnp = aioupnp.upnp.UPNPServer(loop=loop, http=http, httpport=httpport)
@@ -43,18 +45,18 @@ class UPnPctrl(object):
     def shutdown(self):
         self.aioupnp.shutdown()
 
-    async def media_renderer_removed(self, device = None):
+    async def media_renderer_removed(self, device=None):
         self.log.info('media_renderer_removed %s', device)
         self.mediadevices.pop(device.usn, None)
         if self.device:
             if self.device.media.usn == device.usn:
-                if len(self.mediadevices):
+                if self.mediadevices:
                     self.device = list(self.mediadevices.values())[0]
                 else:
                     self.device = None
                 self.trigger_callbacks()
 
-    async def media_renderer_found(self, device = None):
+    async def media_renderer_found(self, device=None):
         if device is None:
             return
 
@@ -74,11 +76,11 @@ class UPnPctrl(object):
         service = device.service('AVTransport')
         await service.subscribe('CurrentTrackMetaData', self.state_variable_change)
         await service.subscribe('TransportState', self.state_variable_change)
- 
+
     async def play(self, url, title='Video', vtype='video/mp4'):
         if self.device:
             try:
-                async with aiohttp.ClientSession(read_timeout = 5) as session:
+                async with aiohttp.ClientSession(read_timeout=5) as session:
                     async with session.head(url) as response:
                         ctype = response.headers.get('content-type', vtype)
                         service = self.device.media.service('AVTransport')
@@ -89,7 +91,7 @@ class UPnPctrl(object):
                         await service.transporturi(url, title, ctype)
                         await service.play()
             except (OSError, asyncio.TimeoutError, aiohttp.client_exceptions.ClientError) as err:
-                self.log.warn('play %s', err)
+                self.log.warning('play %s', err)
                 return
 
     def add_alert_handler(self, callback):
@@ -98,12 +100,12 @@ class UPnPctrl(object):
 
     def remove_alert_handler(self, callback):
         self.registered_callbacks.pop(id(callback), None)
-            
+
     def state_variable_change(self, variable):
         usn = variable.service.device.usn
         if variable.name == 'CurrentTrackMetaData':
             self.log.info('%s changed from %s to %s', variable.name, variable.old_value, variable.value)
-            if variable.value != None and len(variable.value)>0:
+            if variable.value is not None and variable.value:
                 try:
                     elt = aioupnp.dlna.didl.fromString(variable.value)
                     self.mediadevices[usn].status['item'] = []
@@ -132,51 +134,106 @@ class UPnPctrl(object):
                         callback['status'] = status.copy()
                     else:
                         callback['status'] = status
-            except Exception as e:
-                self.log.error('trigger_callbacks exception %s', e)
+            except Exception as exeption:
+                self.log.error('trigger_callbacks exception %s', exeption)
 
-    def refresh(self):
-        self.coherence.msearch.double_discover()
+    async def refresh(self):
+        await self.aioupnp.ssdp.resendMSearch()
 
 
-class Info(object):
+class CancellablePool:
+    def __init__(self, max_workers=3):
+        self._free = {self._new_pool() for _ in range(max_workers)}
+        self._working = set()
+        self._change = asyncio.Event()
+
+    def _new_pool(self):
+        return multiprocessing.Pool(1)
+
+    async def apply(self, fn, *args):
+        """
+        Like multiprocessing.Pool.apply_async, but:
+         * is an asyncio coroutine
+         * terminates the process if cancelled
+        """
+        while not self._free:
+            await self._change.wait()
+            self._change.clear()
+        pool = usable_pool = self._free.pop()
+        self._working.add(pool)
+
+        loop = asyncio.get_event_loop()
+        fut = loop.create_future()
+        def _on_done(obj):
+            loop.call_soon_threadsafe(fut.set_result, obj)
+        def _on_err(err):
+            loop.call_soon_threadsafe(fut.set_exception, err)
+        pool.apply_async(fn, args, callback=_on_done, error_callback=_on_err)
+
+        try:
+            return await fut
+        except asyncio.CancelledError:
+            pool.terminate()
+            usable_pool = self._new_pool()
+        finally:
+            self._working.remove(pool)
+            self._free.add(usable_pool)
+            self._change.set()
+
+    def shutdown(self):
+        for p in self._working | self._free:
+            p.terminate()
+        self._free.clear()
+
+
+class Info:
     def __init__(self, loop):
         self.log = logging.getLogger(self.__class__.__name__)
         self.loop = loop
-        self.ydl = youtube_dl.YoutubeDL(
+
+    @staticmethod
+    def extract_info(url=None):
+        try:
+            ydl = youtube_dl.YoutubeDL(
                 params={
                     'quiet': True,
                     'cachedir': '/tmp/',
                     'youtube_include_dash_manifest': False,
-                    'prefer_ffmpeg': True
+                    'prefer_ffmpeg': True,
+                    'socket_timeout': 5,
+                    'skip_download': True
                 })
-
-    async def youtube_dl(self, url):
-        try:
-            future = self.loop.run_in_executor(None, self.ydl.extract_info, url, False)
-            stream = await asyncio.wait_for(future, 5, loop=self.loop)
-            #format_selector = ydl.build_format_selector('all[height>=480]')
-            #select = list(format_selector(stream.get('formats')))
+            stream = ydl.extract_info(url, False)
             data = {}
             data['src'] = stream.get('extractor')
             data['title'] = stream.get('title')
             data['url'] = stream.get('webpage_url')
             data['bitrate'] = []
-            for i in stream.get('formats',[]):
+            for i in stream.get('formats', []):
                 if i.get('acodec') != 'none' and i.get('vcodec') != 'none':
                     data['bitrate'].append({'url':i.get('url'), 'bitrate':i.get('height') or i.get('format_id')})
             return data
-        except youtube_dl.utils.DownloadError as e:
+        except Exception:
             pass
-        except Exception as e:
-            self.log.error('youtube_dl %s', e)
 
-class WebSocketFactory(object):
-    def __init__(self, loop = None, factory = None, upnp = None, torrent = None, peer = None, local = None, ws = None):
+    async def youtube_dl(self, url):
+        pool = CancellablePool()
+        task = self.loop.create_task(pool.apply(self.extract_info, url))
+        try:
+            return await asyncio.wait_for(task, 60)
+        except Exception as exception:
+            self.log.error('youtube_dl %s', exception)
+        finally:
+            pool.shutdown()
+
+class WebSocketFactory:
+    def __init__(self, loop=None, factory=None, upnp=None, torrent=None, peer=None, local=None, ws=None):
         self.log = logging.getLogger(self.__class__.__name__)
         self._factory = factory
         self._upnp = upnp
         self._torrent = torrent
+        self._upnpupdate = None
+        self._btupdate = None
         self.loop = loop
         self.peer = peer
         self.local = local
@@ -191,7 +248,7 @@ class WebSocketFactory(object):
 
     @property
     def upnp(self):
-        return self.factory._upnp 
+        return self.factory._upnp
 
     @property
     def torrent(self):
@@ -199,22 +256,26 @@ class WebSocketFactory(object):
 
     async def websocket_handler(self, request):
         self.log.info('websocket_handler %s %s', request.remote, request.host)
-        
+
         ws = aiohttp.web.WebSocketResponse()
         await ws.prepare(request)
         wsclient = WebSocketFactory(
-            factory = self.factory,
-            peer = request.remote,
-            local = request.host,
-            ws = ws
+            factory=self.factory,
+            peer=request.remote,
+            local=request.host,
+            ws=ws
         )
         await wsclient.onOpen()
         try:
             async for msg in ws:
                 if msg.type == aiohttp.WSMsgType.TEXT:
-                    await wsclient.onMessage(msg.data, False)
+                    await wsclient.onMessage(msg.data)
                 elif msg.type == aiohttp.WSMsgType.ERROR:
                     break
+        except (OSError, TimeoutError):
+            pass
+        except Exception as e:
+            self.log.error('websocket_handler %s', e)
         finally:
             wsclient.onClose()
 
@@ -231,12 +292,12 @@ class WebSocketFactory(object):
             data['info_hash'] = handle['info_hash']
             data['title'] = handle['title']
             data['files'] = [{
-                    'title': os.path.basename(i),
-                    'url':
-                        urllib.parse.urljoin(
-                            urllib.parse.urlunsplit(['http', self.local, self.torrent.options.get('urlpath'), None, None]),
-                            urllib.parse.quote(i)
-                        )
+                'title': os.path.basename(i),
+                'url':
+                    urllib.parse.urljoin(
+                        urllib.parse.urlunsplit(['http', self.local, self.torrent.options.get('urlpath'), None, None]),
+                        urllib.parse.quote(i)
+                    )
                 } for i in self.videofiles(handle['files'])]
             response.append(data)
         return response
@@ -259,21 +320,21 @@ class WebSocketFactory(object):
     def onClose(self):
         self.log.info('WS client closed %s', self.ws.exception())
         self.factory.wsclients.discard(self)
-        if hasattr(self, '_upnpupdate'):
+        if self._upnpupdate:
             self.upnp.remove_alert_handler(self._upnpupdate)
-        if hasattr(self, '_btupdate'):
+        if self._btupdate:
             self.torrent.remove_alert_handler('files_list_update_alert', self._btupdate)
 
     async def onShutdown(self, app):
         for wsclient in set(self.wsclients):
             await wsclient.ws.close(code=aiohttp.WSCloseCode.GOING_AWAY,
-                message='Server shutdown')
+                                    message='Server shutdown')
 
     async def sendMessage(self, message, request):
         request['response'] = message
         await self.ws.send_json(request)
 
-    async def onMessage(self, payload, isBinary):
+    async def onMessage(self, payload):
         jsondata = json.loads(payload)
         if jsondata.get('action') == 'play':
             data = jsondata.pop('request', {})
@@ -286,7 +347,7 @@ class WebSocketFactory(object):
                 self.log.info('push to play url: %s', url)
                 await self.upnp.play(url, data.get('title', 'Video'))
         elif jsondata.get('action') == 'refresh':
-            self.upnp.refresh()
+            await self.upnp.refresh()
         elif jsondata.get('action') == 'search':
             data = jsondata.pop('request', {})
             url = data.get('url')
@@ -340,7 +401,13 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
     loop = asyncio.get_event_loop()
 
+    def exception_handler(loop, context):
+        logging.warning('loop: %s', context)
+
+    loop.set_exception_handler(exception_handler)
+
     logging.getLogger('SSDPServer').setLevel(logging.INFO)
+    logging.getLogger('EventsServer').setLevel(logging.INFO)
     logging.getLogger('TorrentProducer').setLevel(logging.WARN)
     logging.getLogger('TorrentStream').setLevel(logging.INFO)
 
@@ -349,7 +416,7 @@ def main():
     http = aiohttp.web.Application(middlewares=[rootindex])
     upnp = UPnPctrl(loop=loop, http=http, httpport=httpport)
     torrent = torrentstream.TorrentStream(loop=loop, save_path='/opt/tmp/', urlpath='/bt/')
-    ws = WebSocketFactory(loop = loop, upnp = upnp, torrent = torrent)
+    ws = WebSocketFactory(loop=loop, upnp=upnp, torrent=torrent)
     http.on_shutdown.append(ws.onShutdown)
 
     http.add_subapp(torrent.options['urlpath'], torrent.http)
