00b23ec3379ca401e23ad5460395a67b24272e7a
German Skalaukhov <g.skalaukhov@tinkoff.ru>
2026-09-23 17:47:14 +0300

    feat: добавить диагностику обработки событий GENA

--- aioupnp/__init__.py
+++ aioupnp/__init__.py
@@ -5,9 +5,11 @@ version = '2.0'
 # before creating RendererRegistry).
 from . import _ssdp_compat  # noqa: E402,F401
 from . import _gena_compat  # noqa: E402,F401
+from . import _event_diag  # noqa: E402,F401 - TEMPORARY, see module docstring
 
 _ssdp_compat.apply()
 _gena_compat.apply()
+_event_diag.apply()
 
 from .renderer import RendererRegistry  # noqa: E402
 from .mediaserver import MediaServer  # noqa: E402
